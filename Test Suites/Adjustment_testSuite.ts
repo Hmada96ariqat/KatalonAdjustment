a3b43801188a89da5e0413d60813c364d6afd440
a.areiqat <a.areiqat@areiqat-pc.pio-tech.com>
2021-05-30 10:45:20 +0300

    Test suite

---
+++ Test Suites/Adjustment_testSuite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Adjustment_testSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>477182d7-8c6f-4855-ba20-a7a8ca179a03</testSuiteGuid>
+</TestSuiteEntity>
